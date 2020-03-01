b1eac2df1ff362c368fde180127326736df11ded
John Hillert <john.hillert@gmail.com>
2020-03-01 10:17:09 -0600

    a

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,6 +11,7 @@ module.exports = merge(common, {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, '../build'),
+    publicPath: '/',
   },
   optimization: {
     minimizer: [
