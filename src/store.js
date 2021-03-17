c8f19f3c8cc7a1621f6ed029efcddee9e81ea68c
John Hillert <john.hillert@gmail.com>
2021-03-17 07:50:48 -0500

    ...

--- src/store.js
+++ src/store.js
@@ -7,17 +7,6 @@ const initialState = {};
 
 const middleware = [thunk];
 
-if (nodeEnv !== 'production') {
   const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
-} else {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
-  );
-}
 
 export default store;
