02d0f3f26c3d617f7ae1bdd97bc8f906058ab567
John Hillert <john.hillert@gmail.com>
2021-03-17 04:07:02 -0500

    move into config folder

---
+++ config/webpack.common.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// npm_lifecycle_event
+
+module.exports = {
+    entry: '../src/index.jsx',
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.(js|jsx)$/, include: /node_modules/, use: ['react-hot-loader/webpack'] },
+            { test: /\.less$/, use: ['style-loader', 'css-loader/locals', 'less-loader'] },
+            { test: /\.png$/, use: [{ loader: 'url-loader', options: { mimetype: 'image/png' } }] },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    plugins: [
+        new Dotenv(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'TimeLockr',
+            filename: 'index.html',
+            inject: 'body',
+            favicon: path.resolve(__dirname, '../src/assets/favicon.ico'),
+            template: path.resolve(__dirname, '../src/assets/index.html'),
+            meta: { viewport: 'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no' },
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.css'],
+        alias: {
+            actions: path.resolve(__dirname, '../src/actions/index.js'),
+            components: path.resolve(__dirname, '../src/components/index.js'),
+            config: path.resolve(__dirname, '../config.js'),
+            store: path.resolve(__dirname, '../src/store.js'),
+            theme: path.resolve(__dirname, '../src/theme/index.js'),
+            types: path.resolve(__dirname, '../src/actions/types.js'),
+            utilities: path.resolve(__dirname, '../src/utilities/index.js'),
+        },
+    },
+};
---
+++ config/webpack.prod.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../build'),
+    publicPath: '/',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    },
+  },
+  plugins: [
+    new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en/),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      generateStatsFile: true,
+    }),
+  ],
+});
